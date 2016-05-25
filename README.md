4efbd93418c115cb16453146d93b59fd9b139b5a
mkt <markus.kottlaender@crea.de>
2016-05-25 13:33:48 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Ultimate Checklist
+==================
+
+A Symfony project created on May 24, 2016, 6:56 pm.
---
+++ src/AppBundle/Controller/ChecklistController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\Checklist;
+use AppBundle\Form\ChecklistType;
+
+/**
+ * Checklist controller.
+ *
+ * @Route("/checklist")
+ */
+class ChecklistController extends Controller
+{
+    /**
+     * Lists all Checklist entities.
+     *
+     * @Route("/", name="checklist_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $checklists = $em->getRepository('AppBundle:Checklist')->findAll();
+
+        return $this->render('checklist/index.html.twig', array(
+            'checklists' => $checklists,
+        ));
+    }
+
+    /**
+     * Creates a new Checklist entity.
+     *
+     * @Route("/new", name="checklist_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $checklist = new Checklist();
+        $form = $this->createForm('AppBundle\Form\ChecklistType', $checklist);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($checklist);
+            $em->flush();
+
+            return $this->redirectToRoute('checklist_show', array('id' => $checklist->getId()));
+        }
+
+        return $this->render('checklist/new.html.twig', array(
+            'checklist' => $checklist,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Checklist entity.
+     *
+     * @Route("/{id}", name="checklist_show")
+     * @Method("GET")
+     */
+    public function showAction(Checklist $checklist)
+    {
+        $deleteForm = $this->createDeleteForm($checklist);
+
+        return $this->render('checklist/show.html.twig', array(
+            'checklist' => $checklist,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Checklist entity.
+     *
+     * @Route("/{id}/edit", name="checklist_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Checklist $checklist)
+    {
+        $deleteForm = $this->createDeleteForm($checklist);
+        $editForm = $this->createForm('AppBundle\Form\ChecklistType', $checklist);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($checklist);
+            $em->flush();
+
+            return $this->redirectToRoute('checklist_edit', array('id' => $checklist->getId()));
+        }
+
+        return $this->render('checklist/edit.html.twig', array(
+            'checklist' => $checklist,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Checklist entity.
+     *
+     * @Route("/{id}", name="checklist_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Checklist $checklist)
+    {
+        $form = $this->createDeleteForm($checklist);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($checklist);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('checklist_index');
+    }
+
+    /**
+     * Creates a form to delete a Checklist entity.
+     *
+     * @param Checklist $checklist The Checklist entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Checklist $checklist)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('checklist_delete', array('id' => $checklist->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
