bcad0646398ff96353e29295c465396c914e75a3
mkt <markus.kottlaender@crea.de>
2016-07-06 14:47:05 +0200

    implemented points system

--- src/AppBundle/Twig/AppExtension.php
+++ src/AppBundle/Twig/AppExtension.php
@@ -3,29 +3,39 @@
 namespace AppBundle\Twig;
 
 use AppBundle\Entity\CheckInstance;
+use AppBundle\Entity\CheckInstanceCheck;
 use AppBundle\Entity\Task;
+use AppBundle\Entity\User;
 use Doctrine\ORM\EntityManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class AppExtension extends \Twig_Extension
 {
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
     /**
      * @var EntityManager
      */
     private $em;
 
-    public function __construct(EntityManager $em)
+    public function __construct(ContainerInterface $container)
     {
-        $this->em = $em;
+        $this->container = $container;
+        $this->em = $this->container->get('doctrine.orm.entity_manager');
     }
 
     public function getFilters()
     {
-        return array(
-            new \Twig_SimpleFilter('isChecked', array($this, 'isCheckedFilter')),
-            new \Twig_SimpleFilter('checkedBy', array($this, 'checkedByFilter')),
-            new \Twig_SimpleFilter('checkedDate', array($this, 'checkedDateFilter')),
-            new \Twig_SimpleFilter('checkProgress', array($this, 'checkProgressFilter')),
-        );
+        return [
+            new \Twig_SimpleFilter('isChecked', [$this, 'isCheckedFilter']),
+            new \Twig_SimpleFilter('checkedBy', [$this, 'checkedByFilter']),
+            new \Twig_SimpleFilter('checkedDate', [$this, 'checkedDateFilter']),
+            new \Twig_SimpleFilter('checkProgress', [$this, 'checkProgressFilter']),
+            new \Twig_SimpleFilter('points', [$this, 'pointsFilter']),
+        ];
     }
 
     public function isCheckedFilter(Task $task, CheckInstance $checkInstance)
@@ -71,6 +81,25 @@ class AppExtension extends \Twig_Extension
         }
 
         return round($checked / $checkInstance->getChecklist()->getTasks()->count() * 100);
+    }
+
+    public function pointsFilter(User $user)
+    {
+        $points = 0;
+        $pointsInit = $this->container->getParameter('points.init');
+        $pointsCheck = $this->container->getParameter('points.check');
+
+        $checks = $user->getCheckInstanceChecks();
+        foreach ($checks as $check) {
+            $points += $check->isChecked() ? $pointsCheck : 0;
+        }
+
+        $checkInstances = $user->getCheckInstances();
+        foreach ($checkInstances as $checkInstance) {
+            $points += $this->checkProgressFilter($checkInstance) == 100 ? $pointsInit : 0;
+        }
+
+        return $points;
     }
 
     public function getName()
